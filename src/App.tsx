63446cf8e778e5cb1c46151839a030a666f15f4c
akhvarastyan-web <a.khvarastyan@gmail.com>
2026-06-17 18:40:29 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import './App.scss';
 import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from 'react-router-dom';
 import { Header } from './components/Header/Header';
 import { Footer } from './components/Footer/Footer';
 import { Home } from './HomePage';
